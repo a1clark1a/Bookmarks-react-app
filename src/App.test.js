5eb341acc0d1781ffb02bef077f7d79f8146a236
Anthony Clark Perfecto <a1clark1a@gmail.com>
2020-01-31 12:16:41 -0800

    removed extra files

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(
     <BrowserRouter>
       <App />
     </BrowserRouter>,
     div
   );
-=======
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
->>>>>>> 3468b1388fd32ee1fdc350197628bdca10324b0d
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/BookmarkApp/BookmarkApp.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import BookmarkApp from "./BookmarkApp";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<BookmarkApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
